3ab438ae8f2b78c57a4624fc21257d8eb4b17b14
Chuang Jie Bo <bochuangjie@gmail.com>
2022-02-06 16:50:29 +0800

    final check

--- src/component/BusInfo.js
+++ src/component/BusInfo.js
@@ -6,7 +6,18 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Divider from '@mui/material/Divider';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { CircularProgress } from '@mui/material';
+import { Grid, Box, CircularProgress } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import moment from 'moment';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: 'center',
+	color: theme.palette.text.secondary
+}));
 
 function BusInfo({ code, description }) {
 	const [loading, setLoading] = useState(false);
@@ -42,12 +53,17 @@ function BusInfo({ code, description }) {
 				);
 				setBusArrival(data.Services);
 				setLoading(false);
-			}, 10000);
+			}, 60000);
 			return () => clearInterval(interval);
 		}
 	}, [pause]);
 
-	console.log(busArrival);
+	const removeBusStop = () => {
+		let BusStop = JSON.parse(localStorage.getItem('BusStop'));
+		let filteredBusStop = BusStop.filter((el) => el.BusStopCode !== code);
+		localStorage.setItem('BusStop', JSON.stringify(filteredBusStop));
+		window.location.reload();
+	};
 
 	return (
 		<div>
@@ -60,18 +76,84 @@ function BusInfo({ code, description }) {
 					expandIcon={<ExpandMoreIcon />}
 					aria-controls="panel1a-content"
 					id="panel1a-header">
-					<Typography>{description}</Typography>
+					<Box
+						sx={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							width: '100%'
+						}}>
+						<Typography>{description}</Typography>
+						<CancelIcon onClick={removeBusStop} />
+					</Box>
 				</AccordionSummary>
 				<AccordionDetails sx={{ display: 'flex', justifyContent: 'center' }}>
 					{loading ? (
 						<CircularProgress />
 					) : (
-						<Typography>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-							fugit fuga asperiores blanditiis eos placeat omnis minus, ullam
-							laudantium quae aut sint ab adipisci enim sapiente cupiditate
-							repellendus! Nesciunt, quibusdam?
-						</Typography>
+						<Box sx={{ flexGrow: 1 }}>
+							{busArrival.map((busService) => {
+								return (
+									<Grid
+										container
+										style={{ marginBottom: '1em' }}
+										key={busService.ServiceNo}
+										spacing={2}>
+										<Grid item xs={2} p={0} m={1}>
+											<Item
+												style={{
+													borderRight: '1px solid grey'
+												}}>
+												<Typography variant="body1" component="body1">
+													{busService.ServiceNo}
+												</Typography>
+											</Item>
+										</Grid>
+										<Grid item xs={3}>
+											<Item
+												style={{
+													borderRight: '1px solid grey',
+													borderLeft: '1px solid grey'
+												}}>
+												{busService.NextBus.EstimatedArrival.trim() === ''
+													? 'NA'
+													: moment(Date.now()).from(
+															busService.NextBus.EstimatedArrival,
+															true
+													  )}
+											</Item>
+										</Grid>
+										<Grid item xs={3}>
+											<Item
+												style={{
+													borderRight: '1px solid grey',
+													borderLeft: '1px solid grey'
+												}}>
+												{busService.NextBus2.EstimatedArrival.trim() === ''
+													? 'NA'
+													: moment(Date.now()).from(
+															busService.NextBus2.EstimatedArrival,
+															true
+													  )}
+											</Item>
+										</Grid>
+										<Grid item xs={3}>
+											<Item
+												style={{
+													borderRight: '1px solid grey',
+													borderLeft: '1px solid grey'
+												}}>
+												{busService.NextBus3.EstimatedArrival.trim() === ''
+													? 'NA'
+													: moment(Date.now()).from(
+															busService.NextBus3.EstimatedArrival,
+															true
+													  )}
+											</Item>
+										</Grid>
+									</Grid>
+								);
+							})}
+						</Box>
 					)}
 				</AccordionDetails>
 				<Divider />
